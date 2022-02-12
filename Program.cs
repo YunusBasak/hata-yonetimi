f97604e2a4714f3d7acb9c7e91b5dd7e54d8f1c6
Yunus Basak <yunusbasak0@outlook.com>
2022-02-12 15:06:46 +0300

    hata yönetimi örnekleri yapıldı.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+               // int a = int.Parse(null);
+               // int b = int.Parse("test");
+                int c = int.Parse("-20000000000");
+            }
+            catch(ArgumentNullException ex)
+            {
+                System.Console.WriteLine("Boş değer girdiniz");
+                System.Console.WriteLine(ex);
+                
+            }
+            catch(FormatException ex)
+            {
+                System.Console.WriteLine("Veri tipi uygun değil");
+                System.Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                System.Console.WriteLine("Çok küçük bir değer girdiniz");
+                System.Console.WriteLine(ex);
+            }
+            finally
+            {
+                System.Console.WriteLine("İşlem başarıyla tamamlandı");
+            }
+        }
+    }
+}
